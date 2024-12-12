0800be20c4b30fc2d25d68975f443d53981f5e7d
Demonaindi <demianonaindi919@gmail.com>
2024-12-12 12:41:55 -0300

    vercel insights were implemented

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
+/* eslint-disable no-unused-vars */
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from "@vercel/analytics/react"
 import './index.css'
 import App from './App.jsx'
 
