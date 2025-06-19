f312e51bfa154072f8dc036a8297762f57001488
Demonaindi <169163263+Demonaindi47@users.noreply.github.com>
2025-06-19 10:57:34 -0300

    Vercel analytics was implemented

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import About from "./sections/About";
 import Proyects from "./sections/Proyects";
 import Footer from "./sections/Footer";
 import Experience from "./sections/Experience";
+// eslint-disable-next-line no-unused-vars
+import { Analytics } from "@vercel/analytics/next"
 const app = () => {
   return (
     <main className="max-w-7xl mx-auto">
